c38b7683508c8c96f08a59bf76382a21bf1e8d85
ParamSof <parmeshwarbhargade@gmail.com>
2023-04-04 23:41:22 +0530

    AppConstants is done

---
+++ ElectronicStore/src/main/java/com/Nia/electronic/store/config/AppConstants.java
@@ -0,0 +1,14 @@
+package com.Nia.electronic.store.config;
+
+public class AppConstants {
+
+    public static final String PAGE_NUMBER="0";
+    public static final String PAGE_SIZE="10";
+    public static final String SORT_BY="name";
+    public static final String SORT_DIR="asc";
+    public static final String SORT1_BY="title";
+    public static final String DELETE="Data deleted Successfully";
+}
+
+
+
