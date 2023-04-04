77384b911360f2f9edb6ddd0a5b21cdde6e509db
ParamSof <parmeshwarbhargade@gmail.com>
2023-04-04 23:34:33 +0530

    GlobalExceptionHndler is done

---
+++ ElectronicStore/src/main/java/com/Nia/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.Nia.electronic.store.exceptions;
+
+import com.Nia.electronic.store.dtos.ApiResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+public class GlobalExceptionHandler {
+    private static Logger logger = LoggerFactory.getLogger( GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponseMessage> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+
+        logger.info("Exception Handler invoke !!");
+
+        ApiResponseMessage response = ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.NOT_FOUND).success(true).build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+    public ResponseEntity<Map<String,Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String,Object> response= new HashMap<>();
+        allErrors.stream().forEach(objectError ->{
+            String message = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+            response.put(field, message);
+        });
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+    //handle bad api exception
+    @ExceptionHandler(BadApiRequestException.class)
+    public ResponseEntity<ApiResponseMessage> handleBadApiRequest(BadApiRequestException ex) {
+        logger.info("Bad api request ");
+        ApiResponseMessage response = ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.BAD_REQUEST).success(false).build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
