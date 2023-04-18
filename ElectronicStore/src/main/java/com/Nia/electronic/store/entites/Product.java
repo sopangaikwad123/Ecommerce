a5eaefa95b21eb12a10a66f53ea19f02da63b9a1
ParamSof <parmeshwarbhargade@gmail.com>
2023-04-18 22:56:25 +0530

    Product is done

--- ElectronicStore/src/main/java/com/Nia/electronic/store/entites/Product.java
+++ ElectronicStore/src/main/java/com/Nia/electronic/store/entites/Product.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
 import java.util.Date;
 @Getter
 @Setter
@@ -18,7 +16,8 @@ import java.util.Date;
 @Table(name = "products")
 public class Product {
 @Id
-private String productId;
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long productId;
 private String title;
 
 @Column(length = 1000)
